ee2ae18b416cb1d996008318ea9a8ed0cd3bec95
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-02-20 13:02:36 +0400

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,10 @@
-import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import SinglePost from "./pages/SinglePage"
-import NotFound from "./pages/NotFound"
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/post/:id' element={<SinglePost />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </>
+    <div>
+      <Home/>
+    </div>
   )
 }
 export default App
